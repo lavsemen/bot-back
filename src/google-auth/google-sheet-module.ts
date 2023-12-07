e76af38201c5c3ca0d8d9155163a63e1df0c2ef0
Semen Lavrentev <semen@Semens-MacBook-Pro.local>
2023-12-08 02:33:02 +0300

    first-commit

---
+++ src/google-auth/google-sheet-module.ts
@@ -0,0 +1,40 @@
+import {DynamicModule, Module} from "@nestjs/common";
+import GoogleSheetConnectorDto from "./dto/google-sheet-connector.dto";
+import AsyncGoogleSheetConnectorDto from "./dto/async.google-sheet-connector.dto";
+import {GoogleSheetConnectorService} from "./google-sheet-connector.service";
+import {GoogleAuthService} from "./google-auth-service";
+
+@Module({})
+export class GoogleSheetModule {
+    static register(options: GoogleSheetConnectorDto): DynamicModule {
+        return {
+            module: GoogleSheetModule,
+            providers: [
+                {
+                    provide: 'GOOGLE_SHEET_CONNECTOR',
+                    useValue: options,
+                },
+                GoogleSheetConnectorService,
+                GoogleAuthService
+            ],
+            imports: [],
+            exports: [GoogleSheetConnectorService, GoogleAuthService],
+        }
+    }
+    static registerAsync(options: AsyncGoogleSheetConnectorDto): DynamicModule {
+        return {
+            module: GoogleSheetModule,
+            imports: options.imports,
+            providers: [
+                {
+                    provide: 'GOOGLE_SHEET_CONNECTOR',
+                    useFactory: options.useFactory,
+                    inject: options.inject,
+                },
+                GoogleSheetConnectorService,
+                GoogleAuthService
+            ],
+            exports: [GoogleSheetConnectorService, GoogleAuthService],
+        }
+    }
+}
